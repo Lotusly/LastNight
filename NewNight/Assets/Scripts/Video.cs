dcd1c419a2e398931f4fb114d6fb36cb80eb5667
Lotusly <yul4@andrew.cmu.edu>
2018-06-01 17:28:13 -0400

    actively explore textmesh

--- NewNight/Assets/Scripts/Video.cs
+++ NewNight/Assets/Scripts/Video.cs
@@ -7,35 +7,38 @@ using UnityEngine.Video;
 public class Video : MonoBehaviour
 {
 
-	private RawImage _rawImage;
+	//private RawImage _rawImage;
 
 	private VideoPlayer _videoPlayer;
 
-	private bool flag = true;
+	//private bool flag = true;
 
 	// Use this for initialization
 	void Start ()
 	{
-		_rawImage = GetComponent<RawImage>();
+		//_rawImage = GetComponent<RawImage>();
 		_videoPlayer = GetComponent<VideoPlayer>();
-		
 		_videoPlayer.Prepare();
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (_videoPlayer.isPrepared && !_videoPlayer.isPlaying)
-		{
-			if (flag)
-			{
-				flag = false;
-				_rawImage.texture = _videoPlayer.texture;
-			}
-			_videoPlayer.Play();
-			_videoPlayer.Prepare();
-		}
+	void Update()
+	{
 		
+
+			if (_videoPlayer.isPrepared && !_videoPlayer.isPlaying)
+			{
+
+
+				
+				_videoPlayer.Play();
+				_videoPlayer.Prepare();
+
+
+			}
+
+
+		}
 	
-	}
 }
