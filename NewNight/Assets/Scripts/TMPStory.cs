f8f8d08ecb2459dd1c4731889f30caa3e7d56336
Lotusly <yul4@andrew.cmu.edu>
2018-06-15 17:37:06 -0400

    sdf

--- NewNight/Assets/Scripts/TMPStory.cs
+++ NewNight/Assets/Scripts/TMPStory.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(fileName="TMPStory",menuName="TMPStory",order=1)]
 
-public class TMPStory : ScriptableObject
+[Serializable]public class TMPStory : ScriptableObject
 {
 	
 	public Ui.Dialogue.DialogueContaining[] TmpDialogues;
