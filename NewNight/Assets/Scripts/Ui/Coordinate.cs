dbe0ae5d9fb63bf986f72fbf9428dd7e0c7b464b
unknown <ajjones@umass.edu>
2018-07-24 15:25:15 -0500

    tabs vs spaces

--- NewNight/Assets/Scripts/Ui/Coordinate.cs
+++ NewNight/Assets/Scripts/Ui/Coordinate.cs
@@ -8,18 +8,18 @@ namespace Ui
 	{
 		public Vector3 Space2Screen(Vector3 corSpace, Camera cam = null)
 		{
-            if (cam == null)
-                cam = Camera.main;
+			if (cam == null)
+				cam = Camera.main;
 
-            return cam.WorldToScreenPoint(corSpace);
-        }
+			return cam.WorldToScreenPoint(corSpace);
+		}
 
 		public Vector3 Screen2Space(Vector3 corScreen, Camera cam = null)
 		{
-            if (cam == null)
-                cam = Camera.main;
+			if (cam == null)
+				cam = Camera.main;
 
-            return cam.ScreenToWorldPoint(corScreen);
+			return cam.ScreenToWorldPoint(corScreen);
 		}
 	}
 }
